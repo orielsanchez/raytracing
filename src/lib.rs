a25f89100e70dd3b76ede4245be6c8496b844473
Oriel Sanchez <51418065+orielsanchez@users.noreply.github.com>
2025-03-20 18:01:29 -0700

    rendering surface normals on a sphere

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::fmt::Write as FmtWrite;
 use std::io::Write;
 
 use ray::Ray;
-use vec3::{Color, Point3};
+use vec3::{Color, Point3, Vec3};
 
 pub fn write_color<T: Write>(
     out: &mut T,
@@ -26,20 +26,29 @@ pub fn write_color<T: Write>(
 }
 
 pub fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, &r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, &r);
+    match t > 0.0 {
+        true => {
+            let n = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit_vector();
+            return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
+        }
+        false => {
+            let unit_direction = r.direction().unit_vector();
+            let a = (unit_direction.y() + 1.0) * 0.5;
+            (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
+        }
     }
-    let unit_direction = r.direction().unit_vector();
-    let a = (unit_direction.y() + 1.0) * 0.5;
-    (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
 }
 
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
     let oc = *center - r.origin();
     let a = r.direction().dot(&r.direction());
     let b = -2.0 * r.direction().dot(&oc);
     let c = oc.dot(&oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
 
-    discriminant >= 0.0
+    match discriminant < 0.0 {
+        true => return -1.0,
+        false => return (-b - discriminant.sqrt()) / (2.0 * a),
+    }
 }
