600ff47c75623b69c64012c81568fbc0631fb82b
Oriel Sanchez <51418065+orielsanchez@users.noreply.github.com>
2025-03-20 22:12:03 -0700

    Add front-face tracking to hit record

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,5 +1,7 @@
-use crate::{ray::Ray, vec3::{Point3, Vec3}};
-
+use crate::{
+    ray::Ray,
+    vec3::{Point3, Vec3},
+};
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy)]
@@ -7,6 +9,20 @@ pub struct HitRecord {
     pub p: Point3,
     pub normal: Vec3,
     pub t: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vec3) {
+        // Sets the hit record normal vector
+        // the parameter `outward_normal` is assumed to have unit length.
+
+        self.front_face = r.direction().dot(outward_normal) < 0.0;
+        self.normal = match self.front_face {
+            true => *outward_normal,
+            false => -(*outward_normal),
+        }
+    }
 }
 
 pub trait Hittable {
