03d2f2036da1f99f70edf182391ef35a5f4f1ced
Oriel Sanchez <51418065+orielsanchez@users.noreply.github.com>
2025-03-21 19:31:17 -0700

    Use Gamma correction for accurate color intensity

--- src/lib.rs
+++ src/lib.rs
@@ -12,23 +12,36 @@ use std::io::Write;
 use interval::Interval;
 use rand::Rng;
 
+pub fn linear_to_gamma(linear_component: f64) -> f64 {
+    match linear_component > 0.0 {
+        true => linear_component.sqrt(),
+        false => 0.0,
+    }
+}
+
 pub fn write_color<T: Write>(
     out: &mut T,
     pixel_color: &vec3::Color,
 ) -> Result<usize, std::io::Error> {
     let mut str = String::new();
 
-    let r = pixel_color.x();
-    let g = pixel_color.y();
-    let b = pixel_color.z();
+    let mut r = pixel_color.x();
+    let mut g = pixel_color.y();
+    let mut b = pixel_color.z();
 
+    // Apply linear to gamma transform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
+
+    // translate the [0,1] component values to the byte range [0, 255].
     let intensity = Interval::new(0.000, 0.999);
     let rbyte = (256.0 * intensity.clamp(r)) as i32;
     let gbyte = (256.0 * intensity.clamp(g)) as i32;
     let bbyte = (256.0 * intensity.clamp(b)) as i32;
 
+    // Write out the pixel color components.
     writeln!(str, "{} {} {}", rbyte, gbyte, bbyte).expect("Error formatting write");
-
     out.write(str.as_bytes())
 }
 
